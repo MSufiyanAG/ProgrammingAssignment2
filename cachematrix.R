0a4d1c1e2beceae4d167ad1db5fc8c2f55f28830
MSufiyanAG <msaghori2001@gmail.com>
2021-10-30 07:39:13 +0530

    commit made

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      ##set value of matrix
+      set <- function(y) {
+        x <<- y
+        inv <<- NULL
+      }
+      ##  <<-
+      ##It's the 'superassignment' operator.  It does the assignment in the
+      ##enclosing environment. That is, starting with the enclosing frame, it
+      ##works its way up towards the global environment until it finds a
+      ##variable called , and then assigns to it.   If it never finds
+      ##an existing one it creates one in the global environment.
+      
+      ##get value of matrix
+      get <- function() {x}
+      ##set & get value of inverse
+      setInverse <- function(inverse) {inv <<- inverse}
+      getInverse <- function() {inv}
+      list(set = set,get = get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
@@ -12,4 +30,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+          message("getting cached data")
+          return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
+  }
